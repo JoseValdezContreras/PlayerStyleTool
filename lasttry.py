18bc56262c0f8421e5af3c2d4f1105825b795dc6
JoseValdezContreras <49661661+JoseValdezContreras@users.noreply.github.com>
2026-02-15 14:34:33 -0600

    Update lasttry.py

--- lasttry.py
+++ lasttry.py
@@ -292,9 +292,7 @@ if df is not None:
     # List of famous players to label
     famous_players = [
         'Erling Haaland', 'Neymar', 'Lionel Messi', 'Cristiano Ronaldo', 
-        'Robert Lewandowski', 'Kylian Mbappe-Lottin', 'Antoinne Griezmann', 'Paulo Dybala', 'Bruno Fernandes', 'Marcelo', 'Toni Kroos', 'Ousmane Dembélé', 'Memphis Depay', 'Son Heung-Min', 'Kevin De Bruyne', 'Sergio Ramos',
-        'Karim Benzema', 'Luis Suárez', 'Harry Kane', 'Mohamed Salah
-        'Zlatan Ibrahimovic', 'Gareth Bale', 'Thomas Müller', 'Eden Hazard', 'Gavi', 'Jude Bellingham', 'Eduardo Camavinga', 'Aurelien Tchouameni', 'Jamal Musiala', 'Josko Gvardiol', 'William Saliba', 'Nuno Mendes', 'Nico Williams'
+        'Robert Lewandowski', 'Kylian Mbappe-Lottin', 'Antoinne Griezmann', 'Paulo Dybala', 'Bruno Fernandes', 'Marcelo', 'Toni Kroos', 'Ousmane Dembélé', 'Memphis Depay', 'Son Heung-Min', 'Kevin De Bruyne', 'Sergio Ramos', 'Karim Benzema', 'Luis Suárez', 'Harry Kane', 'Mohamed Salah', 'Zlatan Ibrahimovic', 'Gareth Bale', 'Thomas Müller', 'Eden Hazard', 'Gavi', 'Jude Bellingham', 'Eduardo Camavinga', 'Aurelien Tchouameni', 'Jamal Musiala', 'Josko Gvardiol', 'William Saliba', 'Nuno Mendes', 'Nico Williams'
     ]
     
     # Function to find cluster center representatives
